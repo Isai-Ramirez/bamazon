50bcfb250eca82f83376dae91b9597340a9975b6
isai <Isai4827@4827@gmail.com>
2019-02-21 23:36:01 -0700

    ...

---
+++ bamazon.sql
@@ -0,0 +1,39 @@
+-- droping the data base if it exist that way we don't have conflicts
+DROP DATABASE IF EXISTS bamazondb;
+-- creating the database that we are going to be using 
+CREATE DATABASE bamazondb;
+
+-- specifying which datatbase we are using 
+USE bamazondb:
+
+-- creating the table inside the database that contains all of our information 
+CREATE TABLE products(
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(100) NOT NULL,
+  department_name VARCHAR(45) NOT NULL,
+  price INT default 10.4,
+  stock_quantaty INT default 2,
+  PRIMARY KEY (item_id)
+);
+
+-- putting in the products that im going to sell to customers 
+INSERT INTO products (product_name, department_name, price, stock_quantaty)
+VALUES ("apple watch", "electronics", 250.00, 20);
+INSERT INTO products (product_name, department_name, price, stock_quantaty)
+VALUES ("Beats by DRE", "electronics", 300.00, 20);
+INSERT INTO products (product_name, department_name, price, stock_quantaty)
+VALUES ("mac book", "electronics", 180.00, 20);
+INSERT INTO products (product_name, department_name, price, stock_quantaty)
+VALUES ("playstation 4", "electronics", 340.00, 20);
+INSERT INTO products (product_name, department_name, price, stock_quantaty)
+VALUES ("playstation 2", "electronics", 50.00, 5);
+INSERT INTO products (product_name, department_name, price, stock_quantaty)
+VALUES ("Three leg table", "furnature", 120.00, 20);
+INSERT INTO products (product_name, department_name, price, stock_quantaty)
+VALUES ("Sound mixer", "electronics", 400.00, 20);
+INSERT INTO products (product_name, department_name, price, stock_quantaty)
+VALUES ("Trigonometry Textbook", "Books", 80.00, 20);
+INSERT INTO products (product_name, department_name, price, stock_quantaty)
+VALUES ("Nerf Gun", "Toys", 80.00, 20);
+INSERT INTO products (product_name, department_name, price, stock_quantaty)
+VALUES ("slingshot", "toys", 5.00, 20);
---
+++ bamazonCustomer.js
@@ -0,0 +1,100 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "root",
+    database: "bamazondb"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function (err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    queryAllproducts()
+});
+function queryAllproducts() {
+    connection.query("SELECT * FROM songs", function (err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].price);
+        }
+
+        start();
+    });
+}
+
+// function which prompts the user for what action they should take
+function start() {
+
+    connection.query("SELECT product_name FROM products", function (err, results) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        inquirer
+            .prompt({
+                name: "whichItem",
+                type: "list",
+                message: "What item would you like to buy?",
+                choices: function () {
+                    var choiceArray = [];
+                    for (var i = 0; i < results.length; i++) {
+                        choiceArray.push(results[i].product_name);
+                    }
+                    return choiceArray;
+                },
+                {
+                    name: "purchase",
+                    type: "input",
+                    message: "How many would you like to buy?"
+                }
+            })
+        .then(function (answer) {
+            var chosenItem;
+            for (var i = 0; i < results.length; i++) {
+                if (results[i].item_name === answer.choice) {
+                    chosenItem = results[i];
+
+                }
+            }
+            if (chosenItem.stock_quantaty > parseInt(answer.purchase)) {
+                connection.query(
+                    "UPDATE products SET ? WHERE ?",
+                    {
+                        stock_quantaty: stock_quantaty - answer.purchase
+                    },
+                    {
+                        id: chosenItem.id
+                    },
+                    function (error) {
+                        if (error) throw err;
+                        console.log("Bid placed successfully!");
+                        purchase();
+                    }
+                )
+            } else {
+                console.log("sorry we don't have enough of this item");
+                start();
+            }
+            // couldn't get this function to work so i 
+            function purchase() {
+                connection.query(
+                    "SELECT item_id, price*answer.purchase FROM products",
+                    console.log(query.sql)
+                )
+            }
+        });
+        connection.end();
+    });
+    // unexpected token appears here and if fixed it says no function nor argument is passed inbetween
+}
+
+
+
