8160d67c6baf21caec1a3e8f7f6f441398492b92
isai <Isai4827@4827@gmail.com>
2019-03-19 10:20:43 -0700

    ...

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -22,6 +22,7 @@ connection.connect(function (err) {
     // run the start function after the connection is made to prompt the user
     queryAllproducts()
 });
+
 function queryAllproducts() {
     connection.query("SELECT * FROM songs", function (err, res) {
         for (var i = 0; i < res.length; i++) {
@@ -39,58 +40,60 @@ function start() {
         if (err) throw err;
         // Log all results of the SELECT statement
         inquirer
-            .prompt({
-                name: "whichItem",
-                type: "list",
-                message: "What item would you like to buy?",
-                choices: function () {
-                    var choiceArray = [];
-                    for (var i = 0; i < results.length; i++) {
-                        choiceArray.push(results[i].product_name);
-                    }
-                    return choiceArray;
+            .prompt([
+                {
+                    name: "whichItem",
+                    type: "list",
+                    message: "What item would you like to buy?",
+                    choices: function () {
+                        var choiceArray = [];
+                        for (var i = 0; i < results.length; i++) {
+                            choiceArray.push(results[i].product_name);
+                        }
+                        return choiceArray;
+                    },
                 },
                 {
                     name: "purchase",
                     type: "input",
                     message: "How many would you like to buy?"
                 }
-            })
-        .then(function (answer) {
-            var chosenItem;
-            for (var i = 0; i < results.length; i++) {
-                if (results[i].item_name === answer.choice) {
-                    chosenItem = results[i];
+            ])
+            .then(function (answer) {
+                var chosenItem;
+                for (var i = 0; i < results.length; i++) {
+                    if (results[i].item_name === answer.choice) {
+                        chosenItem = results[i];
 
-                }
-            }
-            if (chosenItem.stock_quantaty > parseInt(answer.purchase)) {
-                connection.query(
-                    "UPDATE products SET ? WHERE ?",
-                    {
-                        stock_quantaty: stock_quantaty - answer.purchase
-                    },
-                    {
-                        id: chosenItem.id
-                    },
-                    function (error) {
-                        if (error) throw err;
-                        console.log("Bid placed successfully!");
-                        purchase();
                     }
-                )
-            } else {
-                console.log("sorry we don't have enough of this item");
-                start();
-            }
-            // couldn't get this function to work so i 
-            function purchase() {
-                connection.query(
-                    "SELECT item_id, price*answer.purchase FROM products",
-                    console.log(query.sql)
-                )
-            }
-        });
+                }
+                if (chosenItem.stock_quantaty > parseInt(answer.purchase)) {
+                    connection.query(
+                        "UPDATE products SET ? WHERE ?",
+                        {
+                            stock_quantaty: stock_quantaty - answer.purchase
+                        },
+                        {
+                            id: chosenItem.id
+                        },
+                        function (error) {
+                            if (error) throw err;
+                            console.log("Bid placed successfully!");
+                            purchase();
+                        }
+                    )
+                } else {
+                    console.log("sorry we don't have enough of this item");
+                    start();
+                }
+                // couldn't get this function to work 
+                function purchase() {
+                    connection.query(
+                        "SELECT item_id, price*answer.purchase FROM products",
+                        console.log(query.sql)
+                    )
+                }
+            });
         connection.end();
     });
     // unexpected token appears here and if fixed it says no function nor argument is passed inbetween
